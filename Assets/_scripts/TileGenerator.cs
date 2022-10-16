e91f86542cd549176872db4750941369e10af92b
AlexeiKonev <96721680+AlexeiKonev@users.noreply.github.com>
2022-10-16 23:18:30 +0300

    fix generator

--- Assets/_scripts/TileGenerator.cs
+++ Assets/_scripts/TileGenerator.cs
@@ -5,29 +5,39 @@ using UnityEngine;
 public class TileGenerator : MonoBehaviour
 {
     public GameObject[] tilePrefabs;
+   [SerializeField] private List<GameObject> tileList;
    [SerializeField] private Transform player;
     public float tileLength = 100;
     public float spawnPos = 0;
     private int startTile = 6;
     private void SpawnTile(int tileIndex)
     {
-        Instantiate(tilePrefabs[tileIndex],transform.forward * spawnPos,transform.rotation);
+     GameObject tempTile = Instantiate(tilePrefabs[tileIndex],transform.forward * spawnPos,transform.rotation);
+       
         spawnPos += tileLength;
+        tileList.Add(tempTile);
+    }
+    private void DeleteTile()
+    {
+        Destroy(tileList[0]);
+        tileList.RemoveAt(0);
     }
     void Start()
     {
       for(int i= 0; i < startTile; i++)
         {
             SpawnTile(Random.Range(0, tilePrefabs.Length));
+
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(player.position.z > spawnPos - (startTile * tileLength))
+        if(player.position.z - 60 > spawnPos - (startTile * tileLength))
         {
             SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
         }
     }
 }
